bc6dec7c263ccef6a286b67bc8eef0c250cca649
Federico Jimenez <fedeejimenez@gmail.com>
2020-04-19 21:23:13 +0100

    Fix vulnerabilities

---
+++ public/precache-manifest.8ae5633cdc374c7d9a6e3c36da67eb0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2510ba7575462bc2f090ab1f3cf72995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1563f41d5de0598c0e82",
+    "url": "/static/css/main.8ff2bf0b.chunk.css"
+  },
+  {
+    "revision": "204822ad5ce3f0354443",
+    "url": "/static/js/2.c42a940e.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.c42a940e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1563f41d5de0598c0e82",
+    "url": "/static/js/main.13c1f94c.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
