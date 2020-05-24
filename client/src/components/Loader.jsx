3f2bb3ba4938abda64f587c187129b772dd98951
Federico Jimenez <fedeejimenez@gmail.com>
2020-05-24 15:21:53 +0100

    Make loader responsive

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,18 +1,42 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import ContentLoader from "react-content-loader";
 
 const Loader = props => {
   return (
-    <ContentLoader speed={2} primaryColor="#f3f3f3" secondaryColor="#ecebeb">
-      <rect x="17%" y="20" rx="5" ry="5" width="16%" height="30" />
+    <ContentLoader speed={2} primary="#f3f3f3" secondary="#ecebeb">
+      <rect
+        x="17%"
+        y="20"
+        rx="5"
+        ry="5"
+        width="16%"
+        height="30"
+        className="Loader-title"
+      />
 
       {new Array(6).fill(" ").map((_, i) => {
         return (
-          <rect x="5%" y={i * 50 + 70} rx="5" ry="5" width="40%" height="30" />
+          <rect
+            x="5%"
+            y={i * 50 + 70}
+            rx="5"
+            ry="5"
+            width="40%"
+            height="30"
+            key={i}
+            className="Loader-text"
+          />
         );
       })}
-      <rect x="55%" y="20" rx="5" ry="5" width="40%" height="80%" />
+      <rect
+        x="55%"
+        y="20"
+        rx="5"
+        ry="5"
+        width="40%"
+        height="80%"
+        className="Loader-image"
+      />
     </ContentLoader>
   );
 };
