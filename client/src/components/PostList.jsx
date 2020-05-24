826aa95547873e81183594a37abd9a60e08b2865
Federico Jimenez <fedeejimenez@gmail.com>
2020-05-24 13:06:47 +0100

    Add lazy load for posts

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -5,6 +5,7 @@ import CreateButton from "./Buttons/CreateButton";
 import HomeButton from "./Buttons/HomeButton";
 import ReactHtmlParser from "react-html-parser";
 import "../stylesheets/PostList.css";
+import LazyLoad from "react-lazyload";
 
 class PostList extends Component {
   constructor() {
@@ -31,51 +32,53 @@ class PostList extends Component {
         <hr />
         {this.state.posts.sort().map((post, index) => {
           return (
-            <div key={post.id}>
-              <section
-                className="PhotoList-item"
-                style={{
-                  flexDirection: index % 2 !== 0 ? "row-reverse" : "row"
-                }}
-              >
-                <div className="PhotoList-content">
-                  <header className="text-center mb-40">
-                    <h3>{post.title}</h3>
-                  </header>
-                  <div className="card-block">
-                    <div className="PhotoList-text">
-                      <div className="text-center">
-                        {" "}
-                        {ReactHtmlParser(post.content)}{" "}
-                      </div>
-                    </div>
-                    <p className="text-center mt-40">
-                      <Link
-                        to={`/posts/${post.id}`}
-                        className="PhotoList-read-more btn btn-primary btn-round"
-                      >
-                        Read more
-                      </Link>
-                    </p>
-                  </div>
-                </div>
-                <div
-                  className="PhotoList-frame"
-                  style={{ backgroundImage: `url(${post.src})` }}
+            <LazyLoad height={200}>
+              <div key={post.id}>
+                <section
+                  className="PhotoList-item"
+                  style={{
+                    flexDirection: index % 2 !== 0 ? "row-reverse" : "row"
+                  }}
                 >
-                  {/* <Link to={`/posts/${post.id}`} className=""> */}
-                  <div className="PhotoList-image">
-                    {/* <img */}
-                    {/* className="" */}
-                    {/* // src="https://source.unsplash.com/qGQNmBE7mYw/400x599" */}
-                    {/* alt="..." */}
-                    {/* /> */}
+                  <div className="PhotoList-content">
+                    <header className="text-center mb-40">
+                      <h3>{post.title}</h3>
+                    </header>
+                    <div className="card-block">
+                      <div className="PhotoList-text">
+                        <div className="text-center">
+                          {" "}
+                          {ReactHtmlParser(post.content)}{" "}
+                        </div>
+                      </div>
+                      <p className="text-center mt-40">
+                        <Link
+                          to={`/posts/${post.id}`}
+                          className="PhotoList-read-more btn btn-primary btn-round"
+                        >
+                          Read more
+                        </Link>
+                      </p>
+                    </div>
                   </div>
-                  {/* </Link> */}
-                </div>
-              </section>
-              <hr />
-            </div>
+                  <div
+                    className="PhotoList-frame"
+                    style={{ backgroundImage: `url(${post.src})` }}
+                  >
+                    {/* <Link to={`/posts/${post.id}`} className=""> */}
+                    <div className="PhotoList-image">
+                      {/* <img */}
+                      {/* className="" */}
+                      {/* // src="https://source.unsplash.com/qGQNmBE7mYw/400x599" */}
+                      {/* alt="..." */}
+                      {/* /> */}
+                    </div>
+                    {/* </Link> */}
+                  </div>
+                </section>
+                <hr />
+              </div>
+            </LazyLoad>
           );
         })}
         <Link to={`/posts/new`}>
